6542c19b6015277ce8d270896d81e151d8d06f2e
Yuresh Majhi <yureshmee@gmail.com>
2023-11-13 12:47:13 +0545

    Created reset password method

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 const User = require("../models/userModel");
 const { emailSender } = require("../utils/emailSender");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 // signup method
 const signup = async (req, res) => {
@@ -67,10 +68,78 @@ const verifyEmail = async (req, res) => {
     user.is_Verified = true;
     await user.save();
 
-    res.status(400).json({ msg: "User successfully verified" });
+    res.status(200).json({ msg: "User successfully verified" });
   } catch (error) {
     return res.status(400).json({ error: error.message });
   }
 };
 
-module.exports = { signup, verifyEmail };
+// forget password
+const forgetPassword = async (req, res) => {
+  const { email } = req.body;
+
+  // check if user exists
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res.status(400).json({ error: "Email is not registered" });
+  }
+
+  // generate token for user
+  const forgetToken = jwt.sign(
+    { userId: user._id },
+    process.env.JWT_SECRET_FORGET,
+    { expiresIn: "1d" }
+  );
+  if (!forgetToken) {
+    return res.status(400).json({ error: "Error generating forgetToken" });
+  }
+
+  // send email with url
+  const url = `http://localhost:${process.env.PORT}/api/user/resetpassword/${forgetToken}`;
+  emailSender({
+    from: "noreply@something.com",
+    to: email,
+    subject: "Reset your password",
+    text: "Please click on the following to reset your password " + url,
+    html: `<a href="${url}"><button>Click to verify</button></a>`,
+  });
+
+  return res
+    .status(200)
+    .json({ msg: "Password reset link has been sent to your email" });
+};
+
+// reset password
+const resetPassword = async (req, res) => {
+  const { token } = req.params;
+  const { password } = req.body;
+  let userId;
+
+  // verify the token
+  jwt.verify(token, process.env.JWT_SECRET_FORGET, (error, decoded) => {
+    if (error) {
+      return res
+        .status(400)
+        .json({ error: "Invalid token or token may have expired" });
+    }
+
+    userId = decoded.userId;
+  });
+
+  // check user
+  const user = await User.findOne({ _id: userId });
+  if (!user) {
+    return res.status(400).json({ error: "User doesnot exist" });
+  }
+
+  // hash the password
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+
+  user.password = hash;
+  await user.save();
+
+  res.status(200).json({ msg: "Password reset successfully" });
+};
+
+module.exports = { signup, verifyEmail, forgetPassword, resetPassword };
--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,10 @@
 const express = require("express");
-const { signup, verifyEmail } = require("../controllers/userController");
+const {
+  signup,
+  verifyEmail,
+  forgetPassword,
+  resetPassword,
+} = require("../controllers/userController");
 
 const router = express.Router();
 
@@ -8,5 +13,11 @@ router.post("/signup", signup);
 
 // verify email
 router.get("/verifyemail/:token", verifyEmail);
+
+// forget password
+router.post("/forgetpassword", forgetPassword);
+
+// reset password
+router.post("/resetpassword/:token", resetPassword);
 
 module.exports = router;
